58d14b2783a0cbebd7491b173cb9b4326ae094bd
HADE001 <kuy508138@gmail.com>
2022-07-12 16:28:23 +0700

    finish

---
+++ script.js
@@ -0,0 +1,14 @@
+item1.onclick= () => {
+    big_img.src = "images/img1.png"
+    circle.style.backgroundColor = '#0170143'
+}
+
+item2.onclick= () => {
+    big_img.src = "images/img2.png"
+    circle.style.backgroundColor = '#eb7495'
+
+}
+item3.onclick= () => {
+    big_img.src = "images/img3.png"
+    circle.style.backgroundColor = '#d752b1'
+}
